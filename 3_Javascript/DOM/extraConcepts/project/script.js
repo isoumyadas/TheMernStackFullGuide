97578b14845413eedc68fc551634aa03d814c316
isoumyadas <sammy8das@gmail.com>
2024-11-30 18:43:55 +0530

    Js: Created a small Project to test my knowledge in Events and DOM

---
+++ 3_Javascript/DOM/extraConcepts/project/script.js
@@ -0,0 +1,23 @@
+const selectedDiv = document.querySelector("#colorChange");
+
+selectedDiv.addEventListener("mouseover", (e) => {
+  selectedDiv.style.backgroundColor = "yellow";
+
+  selectedDiv.addEventListener("mouseout", (e) => {
+    selectedDiv.style.backgroundColor = "lightblue";
+  });
+});
+
+// ====================================================================
+
+const selectInput = document.querySelector("#inputField");
+
+const displayKey = document.createElement("p");
+
+selectInput.addEventListener("keydown", (e) => {
+  const key = e.key;
+  const ctrlKey = e.ctrlKey;
+  console.log(key);
+  displayKey.append(key, ctrlKey);
+  document.body.append(displayKey);
+});
