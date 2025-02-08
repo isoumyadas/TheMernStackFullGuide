e2322c43a95b32005e491ba3731332818a565a0e
isoumyadas <sammy8das@gmail.com>
2025-02-08 22:37:24 +0000

    Praticed questions

--- 5_React/conditionalRendering/src/App.jsx
+++ 5_React/conditionalRendering/src/App.jsx
@@ -55,39 +55,126 @@
 
 // Soluition 3
 
+// export default function App() {
+//   return (
+//     <div>
+//       <Drink name="tea" />
+//       <Drink name="coffee" />
+//     </div>
+//   );
+// }
+
+// function Drink({ name }) {
+//   let part, caffeine, age;
+
+//   if (name === "tea") {
+//     part = "leaf";
+//     caffeine = "15–70 mg/cup";
+//     age = "4,000+ years";
+//   } else if (name === "coffee") {
+//     part = "bean";
+//     caffeine = "80–185 mg/cup";
+//     age = "1,000+ years";
+//   }
+
+//   return (
+//     <section>
+//       <h1>{name}</h1>
+//       <dl>
+//         <dt>Part of plant</dt>
+//         <dd>{part}</dd>
+//         <dt>Caffeine content</dt>
+//         <dd>{caffeine}</dd>
+//         <dt>Age</dt>
+//         <dd>{age}</dd>
+//       </dl>
+//     </section>
+//   );
+// }
+
+// Solution 1:
+// const userData = [{ username: "sam", isLoggedIn: false }];
+
+// export default function App() {
+//   return <UserLoggedIn userData={userData} />;
+// }
+
+// function UserLoggedIn({ userData }) {
+//   return (
+//     <div>
+//       {userData.map((user) => // dont use map for single user!
+//         user.isLoggedIn ? (
+//           <h1 key={user.username}>{`Welcome ${user.username}`}</h1>
+//         ) : (
+//           <h1 key={user.username}>{`Please Log in....`}</h1>
+//         )
+//       )}
+//     </div>
+//   );
+// }
+
+// solution 2
+// const user = { userName: "soumya", isPremiumUser: true };
+// export default function App() {
+//   return <PremiumUserContent userData={user} />;
+// }
+
+// function PremiumUserContent({ userData }) {
+//   return (
+//     <div>
+//       <h1 key={userData.userName}>{`Welcome, ${userData.userName}!`}</h1>
+
+//       <p>
+//         {userData.isPremiumUser && <b>Hello this is premium user content</b>}
+//       </p>
+//     </div>
+//   );
+// }
+
+//solution 3
+
+// export default function App() {
+//   return <DarkMode />;
+// }
+
+// function DarkMode() {
+//   const mode = true;
+//   return (
+//     <div>
+//       <h1>React is awesome</h1>
+//       <p>{mode ? "You're in Dark mode" : "You're in Light Mode"}</p>
+//     </div>
+//   );
+// }
+
+// solution 4
+
+// export default function App() {
+//   return <HasError />;
+// }
+
+// function HasError() {
+//   const hasError = false;
+//   return (
+//     <p style={hasError ? { color: "red" } : { color: "inherit" }}>Hello</p>
+//   );
+// }
+
+// solution 5
+
 export default function App() {
-  return (
-    <div>
-      <Drink name="tea" />
-      <Drink name="coffee" />
-    </div>
-  );
+  return <UserRole />;
 }
 
-function Drink({ name }) {
-  let part, caffeine, age;
+function UserRole() {
+  const userRole = "user";
+  return userRole === "admin" ? <AdminPanel /> : <UserPanel />;
+}
 
-  if (name === "tea") {
-    part = "leaf";
-    caffeine = "15–70 mg/cup";
-    age = "4,000+ years";
-  } else if (name === "coffee") {
-    part = "bean";
-    caffeine = "80–185 mg/cup";
-    age = "1,000+ years";
-  }
+function AdminPanel() {
+  return <h1>Welcome Admin</h1>;
+}
 
-  return (
-    <section>
-      <h1>{name}</h1>
-      <dl>
-        <dt>Part of plant</dt>
-        <dd>{part}</dd>
-        <dt>Caffeine content</dt>
-        <dd>{caffeine}</dd>
-        <dt>Age</dt>
-        <dd>{age}</dd>
-      </dl>
-    </section>
-  );
+function UserPanel() {
+  return <h1>Welcome User</h1>;
 }
