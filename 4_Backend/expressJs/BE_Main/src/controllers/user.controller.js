94edfabd507ca94a587b4a936c234679169daba4
isoumyadas <sammy8das@gmail.com>
2025-04-16 00:22:13 +0530

    Fixed the issues where present in user controller

--- 4_Backend/expressJs/BE_Main/src/controllers/user.controller.js
+++ 4_Backend/expressJs/BE_Main/src/controllers/user.controller.js
@@ -173,8 +173,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
@@ -198,7 +198,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized Request");
   }
 
@@ -233,7 +233,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       .json(
         new ApiResponse(
           200,
-          { accessToken, refreshToken },
+          { accessTokenUser, refreshTokenUser },
           "Access token refreshed"
         )
       );
@@ -378,7 +378,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
           $size: "$subscribers",
         },
         channelsSubscribedToCount: {
-          $size: "subscriberdTo",
+          $size: "$subscriberdTo",
         },
         isSubscribed: {
           $cond: {
