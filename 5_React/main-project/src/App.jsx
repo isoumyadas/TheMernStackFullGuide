0a1179ee2f78388f9f670e5ec4d1a126dd5cd1b6
isoumyadas <sammy8das@gmail.com>
2025-07-02 16:50:49 +0000

    feat: Implemented redux-toolkit config and config appwrite

---
+++ 5_React/main-project/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          console.log("IF");
+          dispatch(login(userData));
+        } else {
+          console.log("ELSE");
+          dispatch(logout());
+        }
+      })
+      .catch((err) => {
+        throw new Error(
+          `Error in authService useEffect :: getCurrentUser => ${err.message}`
+        );
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
+}
+
+export default App;
