c762b0fa2dafa45a52b6df4dc51f5716779661c5
isoumyadas <sammy8das@gmail.com>
2025-10-17 10:57:56 +0000

    completed todo project in react

---
+++ Projects/todo-react/src/App.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ArrowUpRightIcon } from "lucide-react";
+import { v4 as uuidv4 } from "uuid";
+import TodoItem from "./components/TodoItem";
+
+export interface Todo {
+  todo: string;
+  isCompleted: boolean;
+  id?: string;
+}
+
+function App() {
+  const [task, setTask] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  // functions
+
+  function addTask(todo: Todo) {
+    setTask((prevTodo) => [...prevTodo, { id: uuidv4(), ...todo }]);
+    // setTask((prevTodo) => [...prevTodo, { id: uuidv4(), todo, isCompleted }]); // This will only work when you destructure from parameter {todo, isCompleted}: Todo
+  }
+
+  const onAdd = (e) => {
+    e.preventDefault();
+    addTask({ todo: inputValue, isCompleted: false });
+    setInputValue("");
+  };
+
+  // The e.target.value is typically used with input elements (like <input>, <textarea>, or <select>), not buttons, because buttons don't have a value property that changes dynamically.
+
+  return (
+    <div className="bg-gray-800">
+      <div className="container flex flex-col justify-center items-center h-screen mx-auto">
+        <div className="flex gap-5">
+          <Input
+            type="text"
+            placeholder="Your next todo"
+            className="text-white font-bold selection:bg-gray-500"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+          />
+
+          <Button
+            className="cursor-pointer bg-gray-900 hover:bg-black"
+            onClick={onAdd}
+            disabled={inputValue.length > 0 ? false : true}
+          >
+            Add <ArrowUpRightIcon />
+          </Button>
+        </div>
+        {/* TODO: add the component of todoLists | to fix this area  */}
+        <div className="flex justify-start w-100 h-100 my-8 flex-col gap-4 text-white overflow-auto scroll-smooth">
+          {task.map((i) => (
+            <li
+              key={i.id}
+              className="list-none flex items-center w-full justify-between"
+            >
+              <TodoItem todo={i} setTask={setTask} />
+            </li>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <div className="flex gap-3">
+  <Input value={task} onChange={(e) => setTaskEdit(e.target.value)} />
+  <Button
+    className="cursor-pointer"
+    onClick={() => setEditButtonClicked(false)}
+  >
+    
+  </Button>
+</div>; */
+}
---
+++ Projects/todo-react/src/components/TodoItem.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { BookMarkedIcon, PencilIcon, XIcon } from "lucide-react";
+import { Input } from "./ui/input";
+
+const TodoItem = ({ todo, setTask }) => {
+  const [taskEdit, setTaskEdit] = useState(todo.todo); // we gave initial todo of the added todo.
+  const [isEdit, setIsEdit] = useState(false);
+
+  function updateTask(id, todo) {
+    setTask((prevTodo) =>
+      prevTodo.map((prev) => (prev.id === id ? todo : prev))
+    );
+  }
+
+  function onEdit() {
+    if (todo.isCompleted) return;
+
+    if (isEdit) {
+      updateTask(todo.id, { ...todo, todo: taskEdit }); // this mean for particular todo id we only have to change the todo key.
+      setIsEdit(false);
+    } else setIsEdit((prev) => !prev);
+  }
+
+  function deleteTask(id) {
+    setTask((prevTodo) => prevTodo.filter((prev) => prev.id !== id));
+  }
+
+  function toggleComplete(id) {
+    setTask((prevTodo) =>
+      prevTodo.map((prev) =>
+        prev.id === id ? { ...prev, isCompleted: !prev.isCompleted } : prev
+      )
+    );
+  }
+
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+
+  return (
+    <>
+      <div className="flex gap-5">
+        <Input
+          type="checkbox"
+          className="w-5"
+          checked={todo.isCompleted}
+          onChange={toggleCompleted}
+        />
+
+        <input
+          className={`outline-none rounded-lg px-2 ${
+            isEdit
+              ? "border-4 border-black focus:outline-none"
+              : "border-transparent"
+          } ${todo.isCompleted ? "line-through" : ""}`}
+          type="text"
+          value={taskEdit}
+          onChange={(e) => setTaskEdit(e.target.value)}
+          readOnly={!isEdit}
+        />
+      </div>
+      {/* edit functionality */}
+      <div className="flex gap-4">
+        <Button
+          className="w-10 h-10 cursor-pointer bg-transparent"
+          onClick={onEdit}
+        >
+          {isEdit ? <BookMarkedIcon /> : <PencilIcon />}
+        </Button>
+
+        <Button
+          className="w-10 h-10 cursor-pointer bg-transparent"
+          onClick={() => deleteTask(todo.id)}
+        >
+          <XIcon />
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default TodoItem;
