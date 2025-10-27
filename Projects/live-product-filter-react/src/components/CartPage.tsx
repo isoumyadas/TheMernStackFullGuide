de550fdb1020cf01202e748c910b27f3885deab5
isoumyadas <sammy8das@gmail.com>
2025-10-27 20:15:34 +0000

    feat: CartPage and updated ProductCard

---
+++ Projects/live-product-filter-react/src/components/CartPage.tsx
@@ -0,0 +1,70 @@
+import { useCartLength } from "@/utils/store";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
+
+const CartPage = () => {
+  const cartItems = useCartLength((state) => state.cartItems);
+  const updateCartItems = useCartLength((state) => state.updateCart);
+  const addItemToCart = useCartLength((state) => state.addToCart);
+  const removeAllItemsFromCart = useCartLength(
+    (state) => state.removeAllCartCount
+  );
+
+  if (cartItems.length === 0) {
+    return <p> Your Cart is Empty</p>;
+  }
+
+  const totalItemsPrice = cartItems
+    .map((item) => item.price * item.quantity)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  return (
+    <div className="bg-gray-500 min-h-full p-5 flex flex-col gap-9">
+      <div className=" flex gap-9">
+        <label className="font-bold text-white bg-black rounded-full p-2">
+          Your Total Price: ${Math.floor(totalItemsPrice)}
+        </label>
+        <button
+          onClick={() => removeAllItemsFromCart()}
+          className="cursor-pointer text-white bg-red-800 rounded-full p-1"
+        >
+          <TrashIcon />
+        </button>
+      </div>
+      <div className="container mx-auto flex items-center flex-wrap gap-5">
+        {cartItems.map((item) => (
+          <li key={item.id} className="list-none">
+            <Card className="w-sm max-h-full">
+              <CardHeader>
+                <CardTitle>{item.title}</CardTitle>
+              </CardHeader>
+              <CardContent className="flex justify-center">
+                <img className="w-20" src={item.image} alt={item.title} />
+              </CardContent>
+              <CardFooter>
+                <div className="flex justify-between items-center w-full">
+                  <p className="rounded-md bg-blue-400 p-3 font-bold text-white">
+                    ${item.price}
+                  </p>
+                  <div className="flex gap-5 bg-black font-bold rounded-full text-white p-2">
+                    <MinusIcon onClick={() => updateCartItems(item)} />{" "}
+                    {item.quantity}
+                    <PlusIcon onClick={() => addItemToCart(item)} />
+                  </div>
+                </div>
+              </CardFooter>
+            </Card>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
--- Projects/live-product-filter-react/src/components/ProductCard.tsx
+++ Projects/live-product-filter-react/src/components/ProductCard.tsx
@@ -6,10 +6,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { StarIcon } from "lucide-react";
+import { ShoppingCartIcon, StarIcon } from "lucide-react";
 import { type Product } from "@/types";
+import { useCartLength } from "@/utils/store";
 
 const ProductCard = ({ prod }: { prod: Product }) => {
+  // const increaseCartCount = useCartLength((state) => state.increaseCartCount);
+  const addItemToCart = useCartLength((state) => state.addToCart);
+  const handleCart = () => {
+    addItemToCart(prod);
+  };
+
   return (
     <div>
       <Card className="w-2xl">
@@ -21,13 +28,22 @@ const ProductCard = ({ prod }: { prod: Product }) => {
           <img className="w-30" src={prod.image} alt={prod.title} />
         </CardContent>
         <CardFooter className="flex justify-between">
-          <p className="rounded-md bg-blue-400 p-3 font-bold text-white">
-            ${prod.price}
-          </p>
-          <p className="rounded-md bg-black p-3 font-bold text-white flex gap-3">
-            <StarIcon className=" fill-amber-300 text-black" />
-            {prod.rating.rate}
-          </p>
+          <div className="flex gap-5">
+            <p className="rounded-md bg-blue-400 p-3 font-bold text-white">
+              ${prod.price}
+            </p>
+            <p className="rounded-md bg-black p-3 font-bold text-white flex gap-3">
+              <StarIcon className=" fill-amber-300 text-black" />
+              {prod.rating.rate}
+            </p>
+          </div>
+
+          <button
+            className="cursor-pointer flex gap-3 bg-amber-600 p-3 rounded-full font-bold text-white"
+            onClick={handleCart}
+          >
+            <ShoppingCartIcon />
+          </button>
         </CardFooter>
       </Card>
     </div>
